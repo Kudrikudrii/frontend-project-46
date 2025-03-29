5ec6803985d9dad46d44a2be2394a60512997bd0
Sergei Garankin <gudri@bk.ru>
2025-03-29 10:39:09 +0300

    add __tests__/

---
+++ __tests__/index.test.js
@@ -0,0 +1,14 @@
+import getDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url); // Полный путь к текущему файлу
+const __dirname = path.dirname(__filename); // Директория файла
+
+const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
+const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
+
+test('JSON files in Stylish format', () => {
+  expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.txt').trim());
+});
