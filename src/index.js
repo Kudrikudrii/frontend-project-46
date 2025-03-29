6d9840381d15d901115345a8b8f98d40b7292e13
Sergei Garankin <gudri@bk.ru>
2025-03-30 01:11:47 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,9 @@ import stylish from './formatters/stylish.js';
 import treeBuilder from './treeBuilder.js';
 /* eslint-disable no-undef */
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
-// const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
-
-// /home/kudri/frontend-project-46/__fixtures__/file1.json
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
-// json
 const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
-// {
-//   host: 'hexlet.io',
-//   timeout: 50,
-//   proxy: '123.234.53.22',
-//   follow: false
-// }
+
 const getDiff = (filepath1, filepath2) => {
   const fullFilePath1 = getFullPath(filepath1);
   const fullFilePath2 = getFullPath(filepath2);
@@ -25,13 +16,7 @@ const getDiff = (filepath1, filepath2) => {
   const data2 = getData(fullFilePath2);
 
   const dataDiff = treeBuilder(data1, data2);
-  // [
-  //   { status: 'deleted', key: 'follow', value: false },
-  //   { status: 'unchanged', key: 'host', value: 'hexlet.io' },
-  //   { status: 'deleted', key: 'proxy', value: '123.234.53.22' },
-  //   { status: 'changed', key: 'timeout', oldValue: 50, newValue: 20 },
-  //   { status: 'added', key: 'verbose', value: true }
-  // ]
+
   const formatDiff = stylish(dataDiff);
   return formatDiff;
 };
