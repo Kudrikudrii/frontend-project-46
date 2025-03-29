007b50dda7a019b21b415845f1d4765019c7af9d
Sergei Garankin <gudri@bk.ru>
2025-03-29 11:15:41 +0300

    fix eslint errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import getDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
+import { test, expect } from '@jest/globals';
 
 const __filename = fileURLToPath(import.meta.url); // Полный путь к текущему файлу
 const __dirname = path.dirname(__filename); // Директория файла
@@ -9,6 +10,6 @@ const __dirname = path.dirname(__filename); // Директория файла
 const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
 
-test('JSON files in Stylish format', () => {
+test('JSON files stylish format', () => {
   expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.txt').trim());
 });
