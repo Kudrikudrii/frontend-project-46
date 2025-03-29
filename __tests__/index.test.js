e820a06b5001c0d80438e76a4843f83e6413419f
Sergei Garankin <gudri@bk.ru>
2025-03-30 01:07:37 +0300

    add new tests for nested files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,9 +10,15 @@ const __dirname = path.dirname(__filename); // Директория файла
 const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
 
-test('JSON files stylish format', () => {
+test('JSON float files stylish format', () => {
   expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.txt').trim());
 });
-test('yml files stylish format', () => {
+test('JSON nested files stylish format', () => {
+  expect(getDiff(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual(readFile('result2.txt').trim());
+});
+test('yml float files stylish format', () => {
   expect(getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('result.txt').trim());
+});
+test('yml nested files stylish format', () => {
+  expect(getDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toEqual(readFile('result2.txt').trim());
 });
