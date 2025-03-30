f8b1e2745460bece4a1ccc33ce7c77ff3848c253
Sergei Garankin <gudri@bk.ru>
2025-03-30 17:44:41 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parse.js';
-import stylish from './formatters/stylish.js';
+import formatter from './formatters/index.js';
 import treeBuilder from './treeBuilder.js';
-/* eslint-disable no-undef */
-const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
-const extractFormat = (filepath) => path.extname(filepath).slice(1);
-const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
-const getDiff = (filepath1, filepath2) => {
-  const fullFilePath1 = getFullPath(filepath1);
-  const fullFilePath2 = getFullPath(filepath2);
+/* eslint-disable no-undef */
+const getFullPath = (filePath) => path.resolve(process.cwd(), filePath);
+const extractFormat = (filePath) => path.extname(filePath).slice(1);
+const getData = (filePath) => parse(fs.readFileSync(filePath, 'utf-8'), extractFormat(filePath));
+
+const getDiff = (filePath1, filePath2, format = 'stylish') => {
+  const fullFilePath1 = getFullPath(filePath1);
+  const fullFilePath2 = getFullPath(filePath2);
 
   const data1 = getData(fullFilePath1);
   const data2 = getData(fullFilePath2);
 
   const dataDiff = treeBuilder(data1, data2);
 
-  const formatDiff = stylish(dataDiff);
-  return formatDiff;
+  return formatter(format)(dataDiff);
 };
 
 export default getDiff;
