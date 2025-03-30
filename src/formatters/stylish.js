4b5a75a65424f5c8268cceda62447fad72790491
Sergei Garankin <gudri@bk.ru>
2025-03-30 17:40:50 +0300

    fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,59 +1,32 @@
-import _ from 'lodash';
-
-const stringify = (value, depth) => {
-  if (!_.isObject(value)) {
+const formatValue = (value, depth) => {
+  if (typeof value !== 'object' || value === null) {
     return String(value);
   }
   const currentIndent = ' '.repeat(depth * 4);
-  const lines = Object.entries(value).map(
-    ([key, value]) => `${currentIndent}    ${key}: ${stringify(value, depth + 1)}`
-  );
+  const lines = Object.entries(value).map(([key, value]) => `${currentIndent}    ${key}: ${formatValue(value, depth + 1)}`);
 
   return `{\n${lines.join('\n')}\n${currentIndent}}`;
 };
 
-const stylish = (arrData, depth = 1) => {
+const getStylishFormat = (tree, depth = 1) => {
   const indent = ' '.repeat(depth * 4 - 4);
-  const result = arrData.flatMap((key) => {
-    const status = key.status;
-    switch (status) {
+  const result = tree.flatMap((node) => {
+    switch (node.status) {
       case 'nested':
-        return `${indent}    ${key.key}: ${stylish(key.children, depth + 1)}`;
-      case 'deleted':
-        return `${indent}  - ${key.key}: ${stringify(key.value, depth)}`;
+        return `${indent}    ${node.key}: ${getStylishFormat(node.children, depth + 1)}`;
+      case 'removed':
+        return `${indent}  - ${node.key}: ${formatValue(node.value, depth)}`;
       case 'added':
-        return `${indent}  + ${key.key}: ${stringify(key.value, depth)}`;
-      case 'changed':
-        return [
-          `${indent}  - ${key.key}: ${stringify(key.oldValue, depth)}`,
-          `${indent}  + ${key.key}: ${stringify(key.newValue, depth)}`,
-        ].join('\n');
+        return `${indent}  + ${node.key}: ${formatValue(node.value, depth)}`;
+      case 'updated':
+        return [`${indent}  - ${node.key}: ${formatValue(node.oldValue, depth)}`, `${indent}  + ${node.key}: ${formatValue(node.newValue, depth)}`].join('\n');
       case 'unchanged':
-        return `${indent}    ${key.key}: ${stringify(key.value, depth)}`;
+        return `${indent}    ${node.key}: ${formatValue(node.value, depth)}`;
       default:
-        return `Unknown type: ${status}`;
+        return `Unknown type: ${node.status}`;
     }
   });
   return `{\n${result.join('\n')}\n${indent}}`;
 };
 
-export default stylish;
-
-// const stylish = (arrData) => {
-
-//   const result = arrData.flatMap((key) => {
-//     const status = key.status;
-//     switch (status) {
-//       case 'deleted':
-//         return `  - ${key.key}: ${key.value}`;
-//       case 'added':
-//         return `  + ${key.key}: ${key.value}`;
-//       case 'changed':
-//         return [`  - ${key.key}: ${key.oldValue}`, `  + ${key.key}: ${key.newValue}`];
-//       default:
-//         return `    ${key.key}: ${key.value}`;
-//     }
-//   });
-//   return `{\n${result.join('\n')}\n}`;
-// };
-// export default stylish;
+export default getStylishFormat;
