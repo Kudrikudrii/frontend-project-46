fe0fbb64257b9b975c9e07709f641340f3b7524d
Sergei Garankin <gudri@bk.ru>
2025-03-30 17:43:22 +0300

    add tests for plain format and Unknown type cases

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,10 +2,12 @@ import getDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
+import getStylishFormat from '../src/formatters/stylish.js';
+import getPlainFormat from '../src/formatters/plain.js';
 
-const __filename = fileURLToPath(import.meta.url); // Полный путь к текущему файлу
-const __dirname = path.dirname(__filename); // Директория файла
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
@@ -21,4 +23,41 @@ test('yml float files stylish format', () => {
 });
 test('yml nested files stylish format', () => {
   expect(getDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toEqual(readFile('result2.txt').trim());
+});
+test('JSON nested files plain format', () => {
+  expect(getDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'plain')).toEqual(readFile('plainResult.txt').trim());
+});
+test('yml nested files plain format', () => {
+  expect(getDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'plain')).toEqual(readFile('plainResult.txt').trim());
+});
+test('yaml nested files plain format', () => {
+  expect(getDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml'), 'plain')).toEqual(readFile('plainResult.txt').trim());
+});
+describe('getStylishFormat with unknown status', () => {
+  test('should return "Unknown type: ..." for unknown status', () => {
+    const invalidData = [
+      {
+        key: 'testKey',
+        status: 'invalidStatus',
+        value: 'testValue',
+      },
+    ];
+
+    const result = getStylishFormat(invalidData);
+    expect(result).toMatch(/Unknown type: invalidStatus/);
+  });
+});
+describe('getPlainFormat with unknown status', () => {
+  test('should return "Unknown type: ..." for unknown status', () => {
+    const invalidData = [
+      {
+        key: 'testKey',
+        status: 'invalidStatus',
+        value: 'testValue',
+      },
+    ];
+
+    const result = getPlainFormat(invalidData);
+    expect(result).toMatch(/Unknown type: invalidStatus/);
+  });
 });
