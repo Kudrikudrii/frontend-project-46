2318ddec19d17a1b05430aedeea01e6a1d6f945d
Sergei Garankin <gudri@bk.ru>
2025-04-02 21:20:28 +0300

    add test for json format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,10 @@
+import getPlainFormat, { getPaths } from '../src/formatters/plain.js';
+import getStylishFormat from '../src/formatters/stylish.js';
+import { test, expect, describe, it } from '@jest/globals';
 import getDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
 import path from 'path';
-import { test, expect, describe, it } from '@jest/globals';
-import getStylishFormat from '../src/formatters/stylish.js';
-import getPlainFormat, { getPaths } from '../src/formatters/plain.js';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,26 +12,36 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
 
-test('JSON float files stylish format', () => {
+test('JSON diff float files in stylish format', () => {
   expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.txt').trim());
 });
-test('JSON nested files stylish format', () => {
+test('JSON diff nested files in stylish format', () => {
   expect(getDiff(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual(readFile('result2.txt').trim());
 });
-test('yml float files stylish format', () => {
+test('yml diff float files in stylish format', () => {
   expect(getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('result.txt').trim());
 });
-test('yml nested files stylish format', () => {
+test('yml diff nested files in stylish format', () => {
   expect(getDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toEqual(readFile('result2.txt').trim());
 });
-test('JSON nested files plain format', () => {
+test('JSON diff nested files in plain format', () => {
   expect(getDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'plain')).toEqual(readFile('plainResult.txt').trim());
 });
-test('yml nested files plain format', () => {
+test('yml diff nested files in plain format', () => {
   expect(getDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'plain')).toEqual(readFile('plainResult.txt').trim());
 });
-test('yaml nested files plain format', () => {
+test('yaml diff nested files in plain format', () => {
   expect(getDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml'), 'plain')).toEqual(readFile('plainResult.txt').trim());
+});
+
+test('JSON diff nested files in json format', () => {
+  expect(getDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'json')).toEqual(readFile('jsonResult.txt').trim());
+});
+test('yml diff nested files in json format', () => {
+  expect(getDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'json')).toEqual(readFile('jsonResult.txt').trim());
+});
+test('yaml diff nested files in json format', () => {
+  expect(getDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml'), 'json')).toEqual(readFile('jsonResult.txt').trim());
 });
 
 describe('getStylishFormat with unknown status', () => {
