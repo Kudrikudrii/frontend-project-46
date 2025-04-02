906dfad1888f06795b6ea0a125de81bb01fde3ea
Sergei Garankin <gudri@bk.ru>
2025-04-03 00:00:59 +0300

    fix hexlet-check errors

--- eslint.config.js
+++ eslint.config.js
@@ -4,22 +4,22 @@ import js from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'] },
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+      globals: globals.browser,
     },
   },
-
-  js.configs.recommended,
-
   {
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: {
+      js,
       import: importPlugin,
     },
+    extends: ['js/recommended'],
     rules: {
+      'no-shadow': 'off',
       'object-curly-newline': 'off',
     },
   },
