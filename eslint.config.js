0bacde7db56bc84bfb70f3993c616533c2ac23c8
Sergei Garankin <gudri@bk.ru>
2025-04-03 10:58:03 +0300

    fix hexlet-check errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,57 @@
 import globals from 'globals';
-import js from '@eslint/js';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
 export default [
-  js.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: {
-        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
       },
       parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
     },
-    plugins: {
-      import: importPlugin,
-    },
+    plugins: { import: importPlugin },
     rules: {
-      'no-shadow': 'off',
-      'object-curly-newline': 'off',
-      // Явно отключаем все правила от плагина import, которые могут конфликтовать
-      'import/no-unresolved': 'off',
-      'import/named': 'off',
-      'import/namespace': 'off',
-      'import/default': 'off',
-      'import/export': 'off',
+      ...importPlugin.configs.recommended.rules,
+    },
+  },
+  ...compat.extends('airbnb-base'),
+  {
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
     },
   },
 ];
