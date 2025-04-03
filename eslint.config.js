43160cf805f8f354445299db2efa6d100df1a07a
Sergei Garankin <gudri@bk.ru>
2025-04-03 11:42:51 +0300

    fix hexlet-check errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,30 @@
 import globals from 'globals';
-import js from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+// import { Linter } from 'eslint'
 
 export default [
-  // Базовый рекомендованный конфиг ESLint
-  js.configs.recommended,
-
-  // Ваши кастомные настройки
+  stylistic.configs.recommended,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    files: ['**/*.{js,ts,tsx}'],
+  },
+  {
+    ignores: ['dist/'],
+  },
   {
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
+      globals: globals.node,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
-    },
-    plugins: {
-      import: importPlugin,
     },
     rules: {
-      'no-underscore-dangle': [
-        'error',
-        {
-          allow: ['__filename', '__dirname'],
-        },
-      ],
-      'import/extensions': [
-        'error',
-        {
-          js: 'always',
-        },
-      ],
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'no-console': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'object-curly-newline': 'off', // Если нужно отключить
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
-];
+]; // satisfies Linter.Config[]
